8bfb31672edf6a91f1f096aa7d31c3085e66a6be
siwolee <siwolee@student.42seoul.kr>
2025-02-22 18:15:13 +0900

    feat: nav

--- front/app/components/navigation.tsx
+++ front/app/components/navigation.tsx
@@ -1,15 +1,54 @@
+import type { MouseEventHandler } from "react";
 import { Link } from "react-router";
-import { NavigationMenuTrigger } from "./ui/navigation-menu";
+import { cn } from "~/lib/utils";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "~/ui/navigation-menu";
+
+const itemStyle = "list-none margin-10 padding-10 align-center";
+
+const Item = ({
+  title,
+  to,
+  onClick,
+  className = "",
+}: {
+  title: string;
+  to?: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  className?: string;
+}) => {
+  return (
+    <NavigationMenuItem className={cn(itemStyle, className)}>
+      {to ? (
+        <Link to={to}>{title}</Link>
+      ) : onClick ? (
+        <button onClick={onClick}>{title}</button>
+      ) : (
+        <button>{title}</button>
+      )}
+    </NavigationMenuItem>
+  );
+};
 
 const Navigation = () => (
-  <nav>
-    <Link to="/">
-      <h1>WhenRUfree</h1>
-    </Link>
-    <NavigationMenuTrigger>
-      <button>Menu</button>
-    </NavigationMenuTrigger>
-    <Link to="/create"></Link>
+  <nav className="flex w-full max-w-full backdrop-blur h-10 top-0 left-0 bg-red-400/20 gap-4 px-10 z-50 justify-between">
+    <Item title="WhenRUfree" to="/" className="font-black " />
+    <NavigationMenu>
+      <NavigationMenuItem className="list-none bg-transparent">
+        <NavigationMenuTrigger className="bg-transparent hover:bg-accent focus:bg-accent">
+          menu
+        </NavigationMenuTrigger>
+        <NavigationMenuContent className="p-10 flex-col bg-amber-400">
+          <Item title="Create Event" to="/create" />
+          <Item title="infomation" to="/info" />
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+    </NavigationMenu>
   </nav>
 );
 
